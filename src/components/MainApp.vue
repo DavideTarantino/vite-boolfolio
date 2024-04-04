9674ca1ce1022a47fa64d302bce323f67d54756a
DavideTarantino <davidetarantino2005@gmail.com>
2024-04-04 15:38:57 +0200

    setup 2

---
+++ src/components/MainApp.vue
@@ -0,0 +1,28 @@
+<script>
+  export default{
+    name: 'MainApp',
+    data(){
+      return{
+
+      }
+    },
+    methods: {
+
+    },
+    mounted(){
+      
+    }
+  }
+</script>
+
+<template>
+
+  <main>
+    <h1>Stampa dei progetti</h1>
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
